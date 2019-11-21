833b5eb71118d6a8e5a75504d6cf981641196f5e
“ <psowceaarti88@gmail.com>
2019-11-21 09:52:42 +0900

    ピン立ての実装

--- app/src/main/java/com/example/tansaku/MapsActivity.java
+++ app/src/main/java/com/example/tansaku/MapsActivity.java
@@ -2,6 +2,8 @@ package com.example.tansaku;
 
 import androidx.fragment.app.FragmentActivity;
 
+import android.location.Criteria;
+import android.location.LocationManager;
 import android.os.Bundle;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -9,11 +11,21 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import java.util.Locale;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.view.View;
+
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    private LatLng location;
+    private Marker onMarkerClick;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,15 +47,69 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
      * it inside the SupportMapFragment. This method will only be triggered once the user has
      * installed Google Play services and returned to the app.
      */
+
+
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+
         // mMapは private GoogleMap mMap; で宣言しています。
         mMap.setMyLocationEnabled(true);
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(0, 0);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+
+        // 皇居辺りの緯度経度
+        location = new LatLng(35.68, 139.76);
+        // marker 追加
+        mMap.addMarker(new MarkerOptions().position(location).title("Tokyo"));
+
+        //マーカー設定
+        MarkerOptions options = new MarkerOptions();
+        options.position(location);
+        options.title("クラスメソッド株式会社");
+        options.snippet(location.toString());
+
+        // camera 移動
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(location, 10));
+
+        // タップした時のリスナーをセット
+        mMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+            @Override
+            public void onMapClick(LatLng tapLocation) {
+                // tapされた位置の緯度経度
+                location = new LatLng(tapLocation.latitude, tapLocation.longitude);
+                String str = String.format(Locale.US, "%f, %f", tapLocation.latitude, tapLocation.longitude);
+                mMap.addMarker(new MarkerOptions().position(location).title(str));
+
+            }
+        });
+
+        // 長押しのリスナーをセット
+        mMap.setOnMapLongClickListener(new GoogleMap.OnMapLongClickListener() {
+            @Override
+            public void onMapLongClick(LatLng longpushLocation) {
+                LatLng newlocation = new LatLng(longpushLocation.latitude, longpushLocation.longitude);
+                mMap.addMarker(new MarkerOptions().position(newlocation).title(":" + longpushLocation.latitude + " :" + longpushLocation.longitude));
+            }
+        });
+
+        //マーカーの削除
+        mMap.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
+            @Override
+            public boolean onMarkerClick(Marker marker) {
+
+                marker.remove();
+                return true;
+            }
+        });
+
+        //マーカーをセットできるよ
+        final LatLng MELBOURNE = new LatLng(35.68, 139.76);
+        Marker melbourne = mMap.addMarker(new MarkerOptions()
+                .position(MELBOURNE)
+                .title("Melbourne")
+                .snippet("Population: 4,137,400"));
+        melbourne.showInfoWindow();
+
     }
 }
+
